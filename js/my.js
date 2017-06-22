c66e6c0e8ae4ebe7a5e728b4b7c444edf7aa02e5
smallsea2016 <1066328715@qq.com>
2017-06-22 14:13:06 +0800

    my.js修改补提交

--- js/my.js
+++ js/my.js
@@ -1,11 +1,11 @@
 var updateObj = {
     title:"2017-06-22更新公告",
-    content:"1、Ku.min.css增加大图预览的样式。<br>2、移动端1像素边框"
+    content:"1、Ku.min.css增加大图预览的样式。<br>2、移动端1像素边框<br>3、解决ku.min.css修改企微的弹窗样式引起的弹窗不可见bug"
 }
-// if (localStorage.time20170622 === undefined) {
+if (localStorage.time20170622 === undefined) {
     _alert(updateObj.title,updateObj.content);
-    // localStorage.time20170622 = updateObj.title
-// };
+    localStorage.time20170622 = updateObj.title
+};
 
 /*设备检测*/
 // 判断屏幕是否旋转
